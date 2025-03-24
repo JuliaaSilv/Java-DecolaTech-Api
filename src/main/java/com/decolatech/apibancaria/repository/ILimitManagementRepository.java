2d2f2ac3bc3f777d98e98c79074ca8e2903e0008
AnnaJulia <julia210320055@gmail.com>
2025-03-24 20:21:00 -0300

    Projeto funcionando

---
+++ src/main/java/com/decolatech/apibancaria/repository/ILimitManagementRepository.java
@@ -0,0 +1,8 @@
+package com.decolatech.apibancaria.repository;
+
+import com.decolatech.apibancaria.domain.model.LimitManagement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILimitManagementRepository extends JpaRepository<LimitManagement, Long> {
+    LimitManagement findByUserId(Long id);
+}
