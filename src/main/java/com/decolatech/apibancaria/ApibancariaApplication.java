bb80a0649667d126b0622cea14fb81d99e531c67
Anna Julia <163208985+JuliaaSilv@users.noreply.github.com>
2025-03-27 22:11:48 -0300

    Update ApibancariaApplication.java

--- src/main/java/com/decolatech/apibancaria/ApibancariaApplication.java
+++ src/main/java/com/decolatech/apibancaria/ApibancariaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+
 public class ApibancariaApplication {
 
 	public static void main(String[] args) {
